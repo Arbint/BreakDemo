ee198f5ee9478a54e500d4b7ba786f62bc0a0518
Li <jili1@uiwtx.edu>
2024-09-11 15:34:18 -0500

    added weapon switch

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -16,25 +16,39 @@ public class Player : MonoBehaviour
     
     private CharacterController _characterController;
     private ViewCamera _viewCamera;
+    private InventoryComponent _inventoryComponent;
      
     private Animator _animator;
     private float _animTurnSpeed;
     private Vector2 _moveInput;
     private Vector2 _aimInput;
 
-    static int animFwdId = Animator.StringToHash("ForwardAmt");
-    static int animRightId = Animator.StringToHash("RightAmt");
-    static int animTurnId = Animator.StringToHash("TurnAmt");
+    private static readonly int animFwdId = Animator.StringToHash("ForwardAmt");
+    private static readonly int animRightId = Animator.StringToHash("RightAmt");
+    private static readonly int animTurnId = Animator.StringToHash("TurnAmt");
+    private static readonly int SwitchWeaponId = Animator.StringToHash("SwitchWeapon");
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
+        _inventoryComponent = GetComponent<InventoryComponent>();
         _gameplayWidget = Instantiate(gameplayWidgetPrefab);
         _gameplayWidget.MoveStick.OnInputUpdated += MoveInputUpdated;
         _gameplayWidget.AimStick.OnInputUpdated += AimInputUpdated;
+        _gameplayWidget.AimStick.OnInputClicked += SwitchWeapon;
         _viewCamera = Instantiate(viewCameraPrefab);
         _viewCamera.SetFollowParent(transform);
+    }
+
+    private void SwitchWeapon()
+    {
+        _animator.SetTrigger(SwitchWeaponId);
+    }
+
+    public void WeaponSwitchPoint()
+    {
+        _inventoryComponent.EquipNextWeapon();  
     }
 
     private void AimInputUpdated(Vector2 inputVal)
--- Assets/Widgets/GameplayWidget/JoyStick/JoyStick.cs
+++ Assets/Widgets/GameplayWidget/JoyStick/JoyStick.cs
@@ -8,11 +8,13 @@ public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
     public delegate void InputUpdatedDelegate(Vector2 inputVal);
 
     public event InputUpdatedDelegate OnInputUpdated;
+    public event Action OnInputClicked;
     [SerializeField] private RectTransform rangeTransform;
     [SerializeField] private RectTransform thumbStickTransform;
     [SerializeField] private float deadZone = 0.2f;
 
     private float _range;
+    private bool _bWasDragging;
 
     private void Awake()
     {
@@ -24,6 +26,7 @@ public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
     {
         rangeTransform.position = eventData.position;
         thumbStickTransform.position = eventData.position;
+        _bWasDragging = false;
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -31,10 +34,16 @@ public class JoyStick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
         thumbStickTransform.localPosition = Vector2.zero;
         rangeTransform.localPosition = Vector2.zero;
         OnInputUpdated?.Invoke(Vector2.zero);
+        
+        if (!_bWasDragging)
+        {
+           OnInputClicked?.Invoke(); 
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        _bWasDragging = true;
         Vector2 offset = Vector2.ClampMagnitude(eventData.position - eventData.pressPosition, _range);
         thumbStickTransform.position = eventData.pressPosition + offset;
         Vector2 input = offset / _range;
