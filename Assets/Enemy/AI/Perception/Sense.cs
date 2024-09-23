609387ac96f337c7699d17756c4573815f7d696a
Li <jili1@uiwtx.edu>
2024-09-23 17:45:29 -0500

    added forgetting coroutine

--- Assets/Enemy/AI/Perception/Sense.cs
+++ Assets/Enemy/AI/Perception/Sense.cs
@@ -1,14 +1,18 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Sense : MonoBehaviour
 {
     [SerializeField] private bool bDrawDebug = true;
+    [SerializeField] private float forgetTime = 3f;
     
     private static HashSet<Stimuli> _registeredStimuliSet = new HashSet<Stimuli>();
 
     private HashSet<Stimuli> _currentSensibleStimuliSet = new HashSet<Stimuli>();
+
+    private Dictionary<Stimuli, Coroutine> _forgettingCoroutines = new Dictionary<Stimuli, Coroutine>();
+
     public static void RegisterStimuli(Stimuli stimuli)
     {
         _registeredStimuliSet.Add(stimuli);
@@ -43,6 +47,15 @@ public abstract class Sense : MonoBehaviour
             return;
 
         _currentSensibleStimuliSet.Remove(stimuli);
+        
+        Coroutine forgetingCoroutine = StartCoroutine(ForgetStimuli(stimuli));
+        _forgettingCoroutines.Add(stimuli,forgetingCoroutine);
+    }
+
+    private IEnumerator ForgetStimuli(Stimuli stimuli)
+    {
+        yield return new WaitForSeconds(forgetTime);
+        _forgettingCoroutines.Remove(stimuli);
         Debug.Log($"I just lost track of: {stimuli.gameObject.name}");
     }
 
@@ -53,6 +66,14 @@ public abstract class Sense : MonoBehaviour
             return;
 
         _currentSensibleStimuliSet.Add(stimuli);
+        
+        if (_forgettingCoroutines.ContainsKey(stimuli))
+        {
+            StopCoroutine(_forgettingCoroutines[stimuli]);
+            _forgettingCoroutines.Remove(stimuli);
+            return;
+        }
+
         Debug.Log($"I just sensed: {stimuli.gameObject.name}");
     }
 
