92061177bfc7e9e52163c9b52dcf11aca0db316c
Li <jili1@uiwtx.edu>
2024-10-21 17:26:26 -0500

    ability dock now scales

--- Assets/Widgets/Ability/AbilityDock.cs
+++ Assets/Widgets/Ability/AbilityDock.cs
@@ -1,17 +1,56 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
+using System;
 
 public class AbilityDock : Widget, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] AbilityWidget abilityWidgetPrefab;
     [SerializeField] RectTransform rootPanel;
+    [SerializeField] float scaleRange = 150f;
 
     List<AbilityWidget> _abiltyWidgets = new List<AbilityWidget>();
 
     AbilitySystemComponent _abilitySystemComponent;
 
     PointerEventData _touchData;
+    private void Update()
+    {
+        if(_touchData != null)
+        {
+            ArrayScale();
+        }
+        else
+        {
+            ResetScale();
+        }
+    }
+
+    private void ResetScale()
+    {
+        foreach (AbilityWidget abilityWidget in _abiltyWidgets)
+        {
+            abilityWidget.SetScaleAmt(0);
+        }
+    }
+
+    private void ArrayScale()
+    {
+        float touchYPos = _touchData.position.y;
+        foreach(AbilityWidget abilityWidget in _abiltyWidgets)
+        {
+            float widgetYPos = abilityWidget.transform.position.y;
+            float distanceToTouch = Mathf.Abs(widgetYPos - touchYPos);
+            if (distanceToTouch > scaleRange)
+            {
+                abilityWidget.SetScaleAmt(0);
+                continue;
+            }
+
+            float scaleAmt = (scaleRange - distanceToTouch) / scaleRange;
+            abilityWidget.SetScaleAmt(scaleAmt);
+        }
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
--- Assets/Widgets/Ability/AbilityWidget.cs
+++ Assets/Widgets/Ability/AbilityWidget.cs
@@ -10,7 +10,30 @@ public class AbilityWidget : MonoBehaviour
     [SerializeField] Image cooldownImage;
     [SerializeField] float cooldownUpdateInterval = 0.05f;
 
+    [SerializeField] float scaledSize = 1.5f;
+    [SerializeField] float scaledOffset = 100f;
+    [SerializeField] float scaleRate = 20f;
+
+    Vector3 _goalScale = Vector3.one;
+    Vector3 _goalLocalOffset = Vector3.zero;
+
     Ability _ability;
+
+    /// <summary>
+    /// Set the amount of scale
+    /// </summary>
+    /// <param name="amt"> ranges from 0 to 1, 0 means no scaling up, 1 means full scaling up </param>
+    public void SetScaleAmt(float amt)
+    {
+        _goalScale = Vector3.one * (1 + (scaledSize - 1) * amt);
+        _goalLocalOffset = Vector3.left * amt * scaledOffset;
+    }
+
+    private void Update()
+    {
+        rootPanel.transform.localScale = Vector3.Lerp(rootPanel.transform.localScale, _goalScale, Time.deltaTime * scaleRate);
+        rootPanel.transform.localPosition = Vector3.Lerp(rootPanel.transform.localPosition, _goalLocalOffset, Time.deltaTime * scaleRate);
+    }
 
     internal void CastAbility()
     {
